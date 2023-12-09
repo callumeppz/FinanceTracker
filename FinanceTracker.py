fa6f5a356a58697795024d1fb9c12a41a1ffdea6
callumeppz <callumapps991@gmail.com>
2023-12-09 22:23:36 +0000

    first commit

---
+++ FinanceTracker.py
@@ -0,0 +1,12 @@
+import csv
+import gspread
+
+file = "CallumAppsYearlySpending.csv"
+
+with open(file, mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        print(row)
+
+sa = gspread.service_account()
+sh = sa.open("Personal Finances")
